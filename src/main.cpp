e1fd2e089ba169d93c2685db948dbe97f62e1897
Junyan Zhang <jz77@yogi.cs.rice.edu>
2020-08-06 18:37:50 -0500

    new coco branch, testing io

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <mpi.h>
 
 #include <iostream>
+#include <vector>
 
 #include "slash/Slash.h"
 #include "util/Eval.h"
 #include "util/Reader.h"
+#include "util/io.cpp"
+using namespace std;
 
 int main(int argc, char *argv[]) {
   int provided;
@@ -13,23 +16,27 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  const std::string webspam =
-      "/mnt/c/Users/nmeis/Research/SLASH/dataset/webspam/webspam_trigram.svm";
+//   const std::string webspam =
+//       "/mnt/c/Users/nmeis/Research/SLASH/dataset/webspam/webspam_trigram.svm";
 
-  Slash s(24, 4, 64, 12);
+// //Num_tables, k, reservoir size, range pow
+//   Slash s(24, 4, 64, 12);
+// // const string filename, uint64_t numItems, uint64_t batchSize, uint32_t avgDim, uint64_t offset = 0
+//   s.store(webspam, 1000, 10, 4000, 100);
+// //const string filename, uint32_t avgDim, uint64_t numQueries, uint64_t k,  uint64_t offset = 0
+//   auto query = s.topK(webspam, 4000, 100, 32);
 
-  s.store(webspam, 1000, 10, 4000, 100);
+//   Reader r(webspam, 4000);
 
-  auto query = s.topK(webspam, 4000, 100, 32);
+//   auto data = r.readSparse(1100);
 
-  Reader r(webspam, 4000);
+//   unsigned int nlist[] = {1, 4, 8, 16, 32};
 
-  auto data = r.readSparse(1100);
+//   similarityMetric(data.indices, data.values, data.markers, data.indices, data.values,
+//                    data.markers + 100, query, 100, 32, nlist, 5);
 
-  unsigned int nlist[] = {1, 4, 8, 16, 32};
-
-  similarityMetric(data.indices, data.values, data.markers, data.indices, data.values,
-                   data.markers + 100, query, 100, 32, nlist, 5);
+  string coco_file = "../../coco_vectors_350.txt";
+  vector<vector<float>> mat = readvec(coco_file);
 
   MPI_Finalize();
 
