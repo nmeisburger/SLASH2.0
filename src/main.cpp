a6df863c50d7b066700c9ee5033874baa9e9c5dd
Junyan Zhang <jz77@yogi.cs.rice.edu>
2020-09-21 17:12:44 -0500

    fix main

--- src/main.cpp
+++ src/main.cpp
@@ -42,8 +42,8 @@ int main(int argc, char *argv[]) {
   string query_file = "../../small_coco_val.txt";
   uint32_t *result = s.query(query_file);
   cout << "Query id results:" << endl;
-  for (auto i : result) {
-    cout << i << " ";
+  for (int i = 0; i < sizeof(result) < i++) {
+    cout << result[i] << " ";
   }
 
 
