587ef8e3b3e0acdc3cc75c5f09c1b12094a2dbe5
Junyan Zhang <jz77@yogi.cs.rice.edu>
2020-09-25 13:59:57 -0500

    small fix

--- src/lsh/LSH.cpp
+++ src/lsh/LSH.cpp
@@ -97,12 +97,12 @@ uint32_t **LSH::queryReservoirs(uint64_t numItems, uint32_t *hashes) {
       index = query * numTables_ + table;
       loc = table * range_ + hashes[index];
       // cout << "IN LSH: Before row assignment, table # " << table << endl;
-      if (loc > range_ * numTables_) {
-        cout << "LSH Hash range out of big bound: " << loc << endl;
-      }
-      if (hashes[index] > range_) {
-        cout << "Out of table single bound: " << hashes[index] <<endl;
-      }
+      // if (loc > range_ * numTables_) {
+      //   cout << "LSH Hash range out of big bound: " << loc << endl;
+      // }
+      // if (hashes[index] > range_) {
+      //   cout << "Out of table single bound: " << hashes[index] <<endl;
+      // }
       rows[index] = reservoirs_[loc] + 1;
     }
   }
