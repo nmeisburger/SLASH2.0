38de7a8f2e6c75d4c3fc5afcef330f75d69884f6
Junyan Zhang <jz77@yogi.cs.rice.edu>
2020-09-25 15:17:38 -0500

    fixing mean calculation

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
   string coco_file = "../../small_coco_train.txt";
   // vector<vector<float>> mat = readvec(coco_file);
   Slash s(NUM_TABLES, RANGE_POW, RESERVOIR_SIZE, RANGE_POW);
-  s.storevec(coco_file, 10000);
+  s.storevec(coco_file, 1);
   string query_file = "../../small_coco_val.txt";
   vector<uint32_t> result = s.query(query_file);
   cout << "Query id results:" << endl;
